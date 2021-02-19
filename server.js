116433324a7265facbf353d680b439fd793567b6
Will Carter <will.carter@fergusllc.com>
2021-02-19 15:19:53 -0700

    add validation

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const pgUtils = require('./pgUtils.js')
+const states = require('./states.js').states
 
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+const isValid = (stateName) => {
+  return states.reduce((acc, st) => {
+
+    if(states.find(st => st.name === stateName))
+      acc = true
+    
+    return acc
+  }, false)
+}
+
 app.get('/', (req, res, next) => {
-  res.status(200).json(`Hello from nodejs-csv-sqlite3. The current server date/time is: ${new Date()}`)
+  res.status(200).json(`Hello from nodejs-csv-pg. The current server date/time is: ${new Date()}`)
 })
 
 app.post('/getPointGeoJson', async (req, res, next) => {
   const csvUrl = req.body.csvUrl//TODO:validate
   const state = req.body.state//TODO:validate
+  
+  if(!isValid(state)) {
+    res.status(200).json("State name invalid")
+    return
+  }
 
   try {
     const geojson = await pgUtils.getPointGeoJsonFor(csvUrl, state)
@@ -27,9 +43,20 @@ app.post('/getPointGeoJson', async (req, res, next) => {
 app.post('/getCountyGeoJson', async (req, res, next) => {
   const csvUrl = req.body.csvUrl//TODO:validate
   const state = req.body.state//TODO:validate
-  const geojson = await pgUtils.getCountyGeoJsonFor(csvUrl, state)
 
-  res.status(200).json(geojson)
+  if(!isValid(state)) {
+    res.status(200).json("State name invalid")
+    return
+  }
+
+  try {
+    const geojson = await pgUtils.getCountyGeoJsonFor(csvUrl, state)
+    res.status(200).json(geojson)
+  }
+  catch(err) {
+    res.status(200).json(err)
+  }
+
 })
 
 const server = app.listen(4070, () => {
