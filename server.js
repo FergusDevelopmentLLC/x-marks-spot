1803e5242e61f8b992015742b279951512a8231d
Will Carter <will.carter@fergusllc.com>
2021-02-19 18:04:02 -0700

    refactor

--- server.js
+++ server.js
@@ -22,39 +22,39 @@ app.get('/', (req, res, next) => {
 })
 
 app.post('/getPointGeoJson', async (req, res, next) => {
-  const csvUrl = req.body.csvUrl//TODO:validate
-  const state = req.body.state//TODO:validate
   
-  if(!isValid(state)) {
-    res.status(200).json("State name invalid")
-    return
-  }
+  try 
+  {
+    const csvUrl = req.body.csvUrl
+    const state = req.body.state
+    
+    if(!isValid(state)) 
+      throw("Error: State name invalid")
 
-  try {
     const geojson = await pgUtils.getPointGeoJsonFor(csvUrl, state)
     res.status(200).json(geojson)
   }
-  catch(err) {
-    res.status(200).json(err)
+  catch(error) {
+    res.status(200).json(error)
   }
-  
+
 })
 
 app.post('/getCountyGeoJson', async (req, res, next) => {
-  const csvUrl = req.body.csvUrl//TODO:validate
-  const state = req.body.state//TODO:validate
-
-  if(!isValid(state)) {
-    res.status(200).json("State name invalid")
-    return
-  }
 
   try {
+
+    const csvUrl = req.body.csvUrl
+    const state = req.body.state
+
+    if(!isValid(state)) 
+      throw("Error: State name invalid")
+
     const geojson = await pgUtils.getCountyGeoJsonFor(csvUrl, state)
     res.status(200).json(geojson)
   }
-  catch(err) {
-    res.status(200).json(err)
+  catch(error) {
+    res.status(200).json(error)
   }
 
 })
@@ -62,6 +62,3 @@ app.post('/getCountyGeoJson', async (req, res, next) => {
 const server = app.listen(4070, () => {
   console.log('App listening at port %s', server.address().port)
 })
-
-// https://gist.githubusercontent.com/FergusDevelopmentLLC/2d2ef2fe6bf41bb7f10cb7a87efbb803/raw/1aaea6621e64892fd1fc9642bb14a729c892ffe8/animal_hospitals_ca.csv
-// https://gist.githubusercontent.com/FergusDevelopmentLLC/f0ea84b0d4d50604c405f5b74db1b498/raw/863d21945fe993f224ee8ad3771930568568a7c3/category.csv
